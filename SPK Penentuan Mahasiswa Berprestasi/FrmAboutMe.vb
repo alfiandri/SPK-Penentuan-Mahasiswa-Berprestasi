a966119ad457e97ad0565ec4197fdfa8b5f4dc77
alfiandri <alfiandri.putra@gmail.com>
2018-11-04 19:08:50 +0700

    Ngetes revisi frmaboutme

--- SPK Penentuan Mahasiswa Berprestasi/FrmAboutMe.vb
+++ SPK Penentuan Mahasiswa Berprestasi/FrmAboutMe.vb
@@ -1,5 +1,6 @@
 ﻿Public Class FrmAboutMe
 
+	'Form About Programmer
     'Button tutup form
     Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnClose.Click
         Me.Close()
